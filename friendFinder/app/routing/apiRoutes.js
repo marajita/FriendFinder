dd4a9696e90f65a6b6d43b9e1605d7feff3bed3c
marajita <rajita.maharjan@gmail.com>
2019-01-29 23:19:17 -0500

    need to work on readme files

--- friendFinder/app/routing/apiRoutes.js
+++ friendFinder/app/routing/apiRoutes.js
@@ -4,6 +4,8 @@ var path = require("path");
 // Import the list of friend entries
 var friends = require("../data/friends.js");
 
+var friendMatched;
+
 // Export API routes
 module.exports = function(app) {
   // Total list of friend entries
@@ -13,20 +15,23 @@ module.exports = function(app) {
 
   // Add new friend entry
   app.post("/api/friends", function(req, res) {
+    friendMatched = null; // initializing friendMatched as null to begin
     var userInput = req.body; //req is the object and body is the key
     var newScoreArr = userInput.scores;
     console.log(newScoreArr);
+    //
     var totalUserScore = 0;
     newScoreArr.forEach(score => {
       totalUserScore = totalUserScore + parseInt(score);
     });
     console.log(totalUserScore);
 
-    var minScoreDifference = 99999999; //this will keep updating because of line 37
+    var minScoreDifference = 99999999; //this will keep updating because of line 44
     var friendSoFar = null;
     friends.forEach(friend => {
-      // this will be the first array from friends where friend is iterator for the whole friends object
+      // this will be the first array from friends where friend is iterator for the whole friends array object
       var friendScoreArr = friend.scores; // gives the array only for scores from friend object
+
       var totalFriendScore = 0; // initializing totalfriendscore is 0 at the begining
       friendScoreArr.forEach(score => {
         // this will be the second array from friendscorearr where score is the iterator
@@ -36,16 +41,21 @@ module.exports = function(app) {
       var scoreDifference = totalUserScore - totalFriendScore; // substracting totaluserscore-totalfriendscore to get the differences
       scoreDifferenceSquare = Math.pow(scoreDifference, 2); //using power rule to multiply the totaldifference so that the -ve number can be changed to positive
       console.log(friend.userName + " : " + scoreDifferenceSquare);
+
       if (scoreDifferenceSquare < minScoreDifference) {
         // now the minscore is 999999 which is greater than scoreDifferenceSquare so the minimum is scoredifferencesuqare
         minScoreDifference = scoreDifferenceSquare; //this will update the vaule of minScoreDifference to the value of scoreDifferenceSquare
         friendSoFar = friend; // this will update the friendsofar who has the minimumscore to be your best match
       }
     });
+    friendMatched = friendSoFar;
     console.log(friendSoFar);
     console.log("Min :" + minScoreDifference);
-
-    res.json(true);
+    if (friendMatched != null) {
+      res.json(friendMatched);
+    } else {
+      res.json(false);
+    }
   });
 
   app.post("/api/clear", function(req, res) {});
