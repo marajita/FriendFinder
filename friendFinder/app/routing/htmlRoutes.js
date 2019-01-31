51c4e9ff0314786e3d89b1f912f940774a77c22f
marajita <rajita.maharjan@gmail.com>
2019-01-30 22:18:03 -0500

    added default html route

--- friendFinder/app/routing/htmlRoutes.js
+++ friendFinder/app/routing/htmlRoutes.js
@@ -6,6 +6,11 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+  // default to home
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
